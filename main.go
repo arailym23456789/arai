0f7bc99391a44711c6986324de543c55aaf384f9
arailym23456789 <87754739021aa@gmail.com>
2024-05-29 16:31:30 +0500

    chang

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
-func main()  {
-	fmt.Println("dff")
+func home(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("Hello snipetbox"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/",home)
+	log.Println("запуск веб-сервера на http://127.0.0.1:4000")
+	err :=http.ListenAndServe(":4000",mux)
+	log.Fatal(err)
 }
