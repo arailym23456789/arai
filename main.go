8401936073d08a3ada4f941fb9f10c55f1e63285
Raxmankul Baldana <braxmankul@gmail.com>
2024-05-31 16:35:13 +0500

    arai commit

--- main.go
+++ main.go
@@ -1,36 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"strconv"
-)
+...
 
+// Обработчик главной странице.
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	w.Write([]byte("Привет Я Арайлым"))
-}
+	// Инициализируем срез содержащий пути к двум файлам. Обратите внимание, что
+	// файл home.page.tmpl должен быть *первым* файлом в срезе.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод не дозволен", 405)
-		return
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
-
-	w.Write([]byte("Создание новой заметки..."))
 }
+
+...
